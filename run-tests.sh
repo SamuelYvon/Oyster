f1186ffb40fa1580e67248d224abf8ac2fe5f31d
SamuelYvon <samuel.yvon@umontreal.ca>
2020-05-02 12:12:21 -0400

    Maybe...
    Signed-off-by: SamuelYvon <samuel.yvon@umontreal.ca>

--- run-tests.sh
+++ run-tests.sh
@@ -11,7 +11,11 @@ echo "------------------------------------"
 echo "$( whereis gsi)"
 
 for f in $( ls ./tests )
-do
-    echo "Running file $f"
-    "$( whereis gsi | awk '{print $2}')" "./tests/$f"
+ do
+     echo "Running file $f"
+     if [[ "$1" == "travis" ]]; then
+         /usr/local/Cellar/gambit-scheme/4.9.3_2/v4.9.3/bin/gsi "./tests/$f"
+     else
+         "$( whereis gsi | awk '{print $2}')" "./tests/$f"
+     fi
 done
